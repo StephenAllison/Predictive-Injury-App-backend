fddb6c662c9959f775d13ec87396190ca9dd4680
Stephen Allison <stephen.allison.technology@gmail.com>
2018-10-10 17:19:56 -0400

    yah

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const Picture = require("../models/picture");
+// const Picture = require("../models/picture");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
